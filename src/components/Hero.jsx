fc8b7c458a56cec0c6021a622869efffa6601500
satish <satishmadhunala03@gmail.com>
2025-05-16 23:47:52 +0530

    resume drive link changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,7 +69,7 @@ const Hero = () => {
           Contact With Me
         </a>
         <a
-          href="https://drive.google.com/file/d/1ymX-_KdX2xtU138qLaN52Hms88buHnNY/view?usp=sharing"
+          href="https://drive.google.com/file/d/1BqqpYQpyHW_jyuBD8lZNerjWAMYTqe6I/view?usp=drive_link"
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
         >
           Resume
